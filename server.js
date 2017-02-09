0d5b7bb45dc9b8ff64fba75495ed3e0b7f6cbb6b
Michał <michal.walaszczyk@it-solve.pl>
2017-02-09 22:35:58 +0100

    distinct stops

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ router.get('/', function(req, res) {
 router.route('/stops')
 
 	.get(function(req, res) {
-		BusStop.find(function(err, stops) {
+		BusStop.find().distinct('przystanek', function(err, stops) {
 			if (err)
 				res.send(err);
 			res.json(stops);
