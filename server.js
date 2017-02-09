9566226b667e3cd5fe04e8c2168768b7123362cc
Michał <michal.walaszczyk@it-solve.pl>
2017-02-09 22:10:31 +0100

    test

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ var port     = process.env.PORT || 8080;
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://ios:godmode@ds056549.mlab.com:56549/pksczestochowadb');
 
-var Mainbase = require('./app/models/mainbase');
+var Mainbase	= require('./app/models/mainbase');
+var Contact     = require('./app/models/contact');
 
 var router = express.Router();
 
@@ -25,6 +26,34 @@ router.get('/', function(req, res) {
 	res.json({ message: 'Rest API v1' });	
 });
 
+router.route('/contacts')
+
+	.post(function(req, res) {
+		
+		var contact = new Contact();
+		contact.name = req.body.name;
+		contact.surname = req.body.surname;
+		contact.age = req.body.age;
+
+		contact.save(function(err) {
+			if (err)
+				res.send({success: false});
+
+			res.json({success: true});
+		});
+
+		
+	})
+
+	.get(function(req, res) {
+		Contact.find(function(err, contacts) {
+			if (err)
+				res.send(err);
+
+			res.json(contacts);
+		});
+	});
+
 router.route('/stops')
 
 	.get(function(req, res) {
@@ -34,6 +63,8 @@ router.route('/stops')
 			res.json(stops);
 		});
 	});
+
+
 
 app.use('/api', router);
 
